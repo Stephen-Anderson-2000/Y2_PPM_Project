c5ecd4b8c4dd8a4f73b6488b2c3a6e39f2beaa86
Nathan Balfour <nathan.balfour2018@my.ntu.ac.uk>
2020-02-15 14:53:05 +0000

    GUI Development 1.1
    Continued to add more layout pages including carer home and welcome screen. Currently unable to set welcome screen as startup

---
+++ app/src/main/java/com/example/ppm_project/popUp.java
@@ -0,0 +1,28 @@
+package com.example.ppm_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import androidx.annotation.Nullable;
+
+public class popUp extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.what_is_this_pop_up_window);
+
+        //getting width and height based on resolution of device
+        DisplayMetrics dm =  new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width =  dm.widthPixels;
+        int height = dm.heightPixels;
+
+        //setting pop up window size to a percentage of the screen resolution
+        double widthMultiplier = 0.8;
+        double heightMultiplier = 0.6;
+        getWindow().setLayout((int)(width*widthMultiplier),(int)(height*heightMultiplier));
+    }
+}
