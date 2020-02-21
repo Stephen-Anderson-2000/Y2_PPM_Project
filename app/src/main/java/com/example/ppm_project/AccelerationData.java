ab5919ffe6cd1c878fad232ffba92ec52c76bf15
richardstone307 <richard.stone307@gmail.com>
2020-02-21 18:01:14 +0000

    Added methods to calculate variance. May need heavy revision, but the maths is in place for now.

--- app/src/main/java/com/example/ppm_project/AccelerationData.java
+++ app/src/main/java/com/example/ppm_project/AccelerationData.java
@@ -1,4 +1,9 @@
 package com.example.ppm_project;
+import java.util.ArrayList;
+//Using ArrayList, this is an array of non-fixed length.
+//Data read from the csv file will need to be added to an ArrayList
+//This can be done by iterating through each line, parsing the correct
+//part as a double, then using arrayList.add(i).
 
 public class AccelerationData
 {
@@ -13,10 +18,49 @@ public class AccelerationData
 
     //public /*type goes here*/ getAccelData() { return accelerationData; }
 
-    public void analyseData()
+    public void analyseData(ArrayList<Double> xArray, ArrayList<Double> yArray, ArrayList<Double> zArray)
     {
-        // Stub definition. Will need to take the acceleration data and analyse it for the relevant
-        // patterns
+        calcVariance(xArray);
+        calcVariance(yArray);
+        calcVariance(zArray);
+
+    }
+
+    //Calculates the mean of an array by iterating through.
+    //Will be needed to calculate variance.
+    public double calcArrayMean(ArrayList<Double> givenArray) {
+        double total = 0;
+
+        for (int i = 0; i < givenArray.size(); i++) {
+            total = total + givenArray.get(i);
+        }
+
+        double arrayMean = total / givenArray.size();
+        return (arrayMean);
+    }
+
+    //Calculates the variance of an array.
+    public double calcVariance(ArrayList<Double> givenArray) {
+        ArrayList<Double> squaredDifferenceArray = new ArrayList<Double>();
+        double sqDifference;
+        double variance;
+        double currentValue;
+
+        //Calculate the mean of the array first
+        double meanOfArray = calcArrayMean(givenArray);
+
+        //Subtract the mean from each value in the array
+        //Square the result
+        for (int i = 0; i < givenArray.size(); i++ ) {
+            currentValue = (givenArray.get(i) - meanOfArray);
+            sqDifference = currentValue * currentValue;
+            squaredDifferenceArray.add(sqDifference);
+        }
+
+        //Average of those squared differences = variance
+        variance = calcArrayMean(squaredDifferenceArray);
+
+        return (variance);
     }
 
 }
