600ecde470b59500c612c9227ee5f8e0e1ddc3e6
richardstone307 <richard.stone307@gmail.com>
2020-02-25 16:15:07 +0000

    Added the calibration class and relevant functions. Updated AccelerationData Both need heavy revision and completion

--- app/src/main/java/com/example/ppm_project/AccelerationData.java
+++ app/src/main/java/com/example/ppm_project/AccelerationData.java
@@ -20,9 +20,9 @@ public class AccelerationData
 
     public boolean isPatientHavingEpisode()
     {
-        System.out.println("X Variance: " + calcVariance(xArray));
-        System.out.println("Y Variance: " + calcVariance(yArray));
-        System.out.println("Z Variance: " + calcVariance(zArray));
+        //System.out.println("X Variance: " + calcVariance(xArray));
+        //System.out.println("Y Variance: " + calcVariance(yArray));
+        //System.out.println("Z Variance: " + calcVariance(zArray));
 
         // Must now compare against a 'threshold' value.
         // The variance of the data gathered was:
@@ -32,16 +32,30 @@ public class AccelerationData
 
         // Perhaps somewhere around 0.2 as a minimum threshold for erratic movement?
 
-        if (calcVariance(xArray) > 0.2 && calcVariance(yArray) > 0.2 && calcVariance(zArray) > 0.2){
+        //0.2 will need to be replaced when calculateThreshold has been implemented
+        if (calcVariance(calculateMagnitude(xArray, yArray, zArray), 0, calculateMagnitude(xArray, yArray, zArray).size()) > 0.2){
             return true;
         } else {
             return false;
         }
     }
+    // Calculates √(x² + y² + z²) so that we're working with a single array.
+    // In theory, this is the magnitude of the vector, showing acceleration in all directions.
+    public ArrayList<Double> calculateMagnitude(ArrayList<Double> x, ArrayList<Double> y, ArrayList<Double> z) {
+        ArrayList<Double> magnitudeOfAccel = new ArrayList<>();
+
+        for (int i = 0; i < x.size() - 1; i++) {
+            double toBeInserted = Math.sqrt((x.get(i)*x.get(i)) + (y.get(i)*y.get(i)) + (z.get(i)*z.get(i)));
+            magnitudeOfAccel.add(toBeInserted);
+        }
+
+        return(magnitudeOfAccel);
+    }
+
 
     // Calculates the mean of an array by iterating through.
     // Will be needed to calculate variance.
-    public double calcArrayMean(ArrayList<Double> givenArray) {
+    public double calcArrayMean(ArrayList<Double> givenArray, int startValue, int endValue) {
         double total = 0;
 
         for (int i = 0; i < givenArray.size(); i++) {
@@ -53,25 +67,25 @@ public class AccelerationData
     }
 
     // Calculates the variance of an array.
-    public double calcVariance(ArrayList<Double> givenArray) {
+    public double calcVariance(ArrayList<Double> givenArray, int startValue, int endValue) {
         ArrayList<Double> squaredDifferenceArray = new ArrayList<Double>();
         double sqDifference;
         double variance;
         double currentValue;
 
-        // Calculate the mean of the array first
-        double meanOfArray = calcArrayMean(givenArray);
+        //Calculate the mean of the array first
+        double meanOfArray = calcArrayMean(givenArray, startValue, endValue);
 
-        // Subtract the mean from each value in the array
-        // Square the result
-        for (int i = 0; i < givenArray.size(); i++ ) {
+        //Subtract the mean from each value in the array
+        //Square the result
+        for (int i = 0 + startValue; i < endValue; i++ ) {
             currentValue = (givenArray.get(i) - meanOfArray);
             sqDifference = currentValue * currentValue;
             squaredDifferenceArray.add(sqDifference);
         }
 
-        // Average of those squared differences = variance
-        variance = calcArrayMean(squaredDifferenceArray);
+        //Average of those squared differences = variance
+        variance = calcArrayMean(squaredDifferenceArray, startValue, endValue);
 
         return (variance);
     }
